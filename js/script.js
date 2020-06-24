e4b020898f67f9e26b9c6106916ec09a73d1e294
Corrado <corradoct@hotmail.it>
2020-06-24 16:18:56 +0200

    Ho completato il secondo punto

--- js/script.js
+++ js/script.js
@@ -6,9 +6,34 @@ $(document).ready(
       "eta" : 35
     };
     // console.log(studente);
-    
+
     for (var key in studente) {
       console.log(studente[key]);
+    }
+
+    var studenti = [
+      {
+        "nome" : "Corrado",
+        "cognome" : "Caruso",
+        "eta" : 35
+      },
+      {
+        "nome" : "Andrea",
+        "cognome" : "Rossi",
+        "eta" : 33
+      },
+      {
+        "nome" : "Marco",
+        "cognome" : "Bianchi",
+        "eta" : 25
+      }
+    ];
+    // console.log(studenti);
+
+    for (var i = 0; i < studenti.length; i++) {
+      var singoloStudenteNome = studenti[i].nome;
+      var singoloStudenteCognome = studenti[i].cognome;
+      console.log(singoloStudenteNome + ' ' + singoloStudenteCognome);
     }
 
 
